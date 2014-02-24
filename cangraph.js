eb934e09eeed76a6a66033d1b00c2ed7c94e9da4
Gabriel <gabriel@abrightconcept.com>
2014-02-24 18:29:09 -0500

    Add main doc block

---
+++ cangraph.js
@@ -0,0 +1,8 @@
+/**
+ * Cangraph
+ *
+ * Graphing and function plotting for the HTML5 canvas. Main function plotting
+ * engine was taken from the link below and modified.
+ *
+ * @link http://www.javascripter.net/faq/plotafunctiongraph.htm
+ */
